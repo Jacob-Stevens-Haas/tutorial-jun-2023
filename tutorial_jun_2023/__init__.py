251572e4d72b9d665ed997af1bc673429e7f220a
Jake Stevens-Haas <37048747+Jacob-Stevens-Haas@users.noreply.github.com>
2023-06-27 00:38:39 -0700

    CLN: Refactor and mess with the code to obscure stuff
    Accidentally made it cleaner though... XD

--- tutorial_jun_2023/__init__.py
+++ tutorial_jun_2023/__init__.py
@@ -16,22 +16,18 @@ def gridsearch(
     skinny_specs: SkinnySpecs = None,
 ):
     curr_other_params = copy(other_params)
+    gridpoint_selector = []
     if skinny_specs is not None:
         ind_skinny, thin_slices = skinny_specs
     else:
         ind_skinny = tuple(range(len(grid_params)))
         thin_slices = None
     full_results_shape = tuple(len(grid) for grid in grid_vals)
+    ndindexer = np.ndindex(full_results_shape)
     full_results = np.empty(full_results_shape)
     full_results.fill(-np.inf)
-    # foo2: gridpoint_selector
-    shape = full_results.shape
-    ndindexer = np.ndindex(shape)
-    gridpoint_selector = []
-    def ind_checker(multi_index):
-        """Check if a multi_index meets thin index criteria"""
+    for multi_index in ndindexer:
         matches = []
-        # check whether multi_index matches criteria of any thin_axis
         for ax1, where_others in zip(ind_skinny, thin_slices, strict=True):
             other_axes = list(ind_skinny)
             other_axes.remove(ax1)
@@ -41,17 +37,10 @@ def gridsearch(
                 if callable(slice_ind):
                     slice_ind = slice_ind(multi_index[ax1])
                 # would check: "== slice_ind", but must allow slice_ind = -1
-                match *= (multi_index[ax2] == range(shape[ax2])[slice_ind])
+                match *= (multi_index[ax2] == range(full_results_shape[ax2])[slice_ind])
             matches.append(match)
-        return any(matches)
-
-    try:
-        while True:
-            ind = next(ndindexer)
-            if ind_checker(ind):
-                gridpoint_selector.append(ind)
-    except StopIteration:
-        pass
+        if any(matches):
+            gridpoint_selector.append(multi_index)
     for ind in gridpoint_selector:
         for axis_ind, key, val_list in zip(ind, grid_params, grid_vals):
             curr_other_params[key] = val_list[axis_ind]
@@ -66,9 +55,9 @@ def gridsearch(
     max_axes = (ind for ind in range(full_results.ndim) if ind not in skinny_specs[0])
     return full_results.max(axis = tuple(max_axes))
 
-class Experiment:
-    pass
 
+# Creating classes in a single line
+Experiment = type("Experiemnt", (), {})
 AbsoluteError = type("AbsoluteError", (Experiment,), {})
 SquaredError = type("SquaredError", (Experiment,), {})
 
